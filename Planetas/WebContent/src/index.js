291acd71b5a8b1504a17126c7950a53fee9bc39c
S0NES <scarabelli.vinicius@gmail.com>
2021-04-30 18:02:33 -0300

    home almost finished

--- Planetas/WebContent/src/index.js
+++ Planetas/WebContent/src/index.js
@@ -1,31 +1,10 @@
 let size = 8;
 
 const listContainer = document.querySelector(".listContainer");
+const listItems = document.querySelectorAll(".listItem");
 
-for (let i = 0; i < size; i++) {
-  const listItem = document.createElement("div");
-  const icon = document.createElement("img");
-  const name = document.createElement("p");
-  const btnEdit = document.createElement("button")
-  const btnDel = document.createElement("button");
-  let random = Math.floor(Math.random() * 6) + 1;
-
-
-  listItem.classList.add("listItem");
-
-  icon.setAttribute("src", `src/img/planet-${random}.png`);
-  icon.setAttribute("id", "icon");
-  icon.setAttribute("alt", "icon");
-  
-  name.setAttribute("id", "name");
-  name.innerText = "Name Planet";
-
-  btnEdit.setAttribute("id", "edit");
-  btnDel.setAttribute("id", "del");
-  
-  listItem.appendChild(icon);
-  listItem.appendChild(name);
-  listItem.appendChild(btnEdit);
-  listItem.appendChild(btnDel);
-  listContainer.appendChild(listItem);
-}
+listItems.forEach((listItem) => {
+  listItem.addEventListener("click", () => {
+    listItem.classList.toggle("active");
+  });
+});
