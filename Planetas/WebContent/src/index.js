001e699c76402e983fc1a67e94a6bae41e4696cb
Chalps <pedrochamar12@gmail.com>
2021-04-30 17:11:33 -0300

    css feito

--- Planetas/WebContent/src/index.js
+++ Planetas/WebContent/src/index.js
@@ -3,24 +3,7 @@ let size = 8;
 const container = document.querySelector(".container");
 
 for (let i = 0; i < size; i++) {
-  let item = document.createElement("div");
-  let img = document.createElement("img");
-  let p = document.createElement("p");
-  let buttonEdit = document.createElement("button");
-  let buttonDelete = document.createElement("button");
-  let random = Math.floor(Math.random() * 6)+1;
-  
-  item.classList.add("item");
-  img.classList.add("icon");
-  p.classList.add("name");
-  p.innerText = "Planet Name";
-  buttonEdit.classList.add("edit");
-  buttonDelete.classList.add("delete");
-  img.setAttribute("src", "src/img/planet-"+random+".png")
-  
-  item.appendChild(img);
-  item.appendChild(p);
-  item.appendChild(buttonEdit);
-  item.appendChild(buttonDelete);
-  container.appendChild(item);
+  let element = document.createElement("div");
+  element.classList.add("item");
+  container.appendChild(element);
 }
