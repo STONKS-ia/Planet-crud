ad7eeed6966577eaad08d363f5021c7f03c40a3b
S0NES <scarabelli.vinicius@gmail.com>
2021-04-30 14:54:20 -0300

    list

--- Planetas/WebContent/src/index.js
+++ Planetas/WebContent/src/index.js
@@ -1,9 +1,31 @@
 let size = 8;
 
-const container = document.querySelector(".container");
+const listContainer = document.querySelector(".listContainer");
 
 for (let i = 0; i < size; i++) {
-  let element = document.createElement("div");
-  element.classList.add("item");
-  container.appendChild(element);
+  const listItem = document.createElement("div");
+  const icon = document.createElement("img");
+  const name = document.createElement("p");
+  const btnEdit = document.createElement("button")
+  const btnDel = document.createElement("button");
+  let random = Math.floor(Math.random() * 6) + 1;
+
+
+  listItem.classList.add("listItem");
+
+  icon.setAttribute("src", `src/img/planet-${random}.png`);
+  icon.setAttribute("id", "icon");
+  icon.setAttribute("alt", "icon");
+  
+  name.setAttribute("id", "name");
+  name.innerText = "Name Planet";
+
+  btnEdit.setAttribute("id", "edit");
+  btnDel.setAttribute("id", "del");
+  
+  listItem.appendChild(icon);
+  listItem.appendChild(name);
+  listItem.appendChild(btnEdit);
+  listItem.appendChild(btnDel);
+  listContainer.appendChild(listItem);
 }
